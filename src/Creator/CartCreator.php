7b5a300dea662a0082fcd0a39688d762a4829581
Matthieu Rolland <matthieurolland@MacBook-Pro-de-Matthieu.local>
2024-05-22 13:25:25 +0200

    work in progress

--- src/Creator/CartCreator.php
+++ src/Creator/CartCreator.php
@@ -4,14 +4,19 @@ namespace PrestaShop\Module\PsFixturesCreator\Creator;
 
 use Cart;
 use Faker\Generator as Faker;
+use PrestaShop\PrestaShop\Core\CommandBus\CommandBusInterface;
+use PrestaShop\PrestaShop\Core\Domain\Cart\Command\AddProductToCartCommand;
 
 class CartCreator
 {
     protected Faker $faker;
 
-    public function __construct(Faker $faker)
+    protected CommandBusInterface $commandBus;
+
+    public function __construct(Faker $faker, CommandBusInterface $commandBus)
     {
         $this->faker = $faker;
+        $this->commandBus = $commandBus;
     }
 
     public function generate(int $number, array $productIds): void
@@ -23,14 +28,29 @@ class CartCreator
 
     public function createCart(array $productIds): Cart
     {
-        $cart = new Cart();
-        $cart->id_currency = 1;
-        $cart->add();
+        $cart = new Cart(17);
+        /*$cart->id_currency = 1;
+        $cart->id_customer = 3;
+        $cart->add();*/
 
-        for ($i = 0; $i < $this->faker->numberBetween(1, 5); ++$i) {
-            $randomProduct = $this->faker->randomElement($productIds);
-            $cart->updateQty($this->faker->numberBetween(1, 3), $randomProduct['id_product']);
+        $numberOfProducts = 1200;
+
+        for ($i = 630; $i < $numberOfProducts; $i++) {
+            $this->commandBus->handle(new AddProductToCartCommand(
+                $cart->id,
+                $i,
+                1,
+                 null,
+                []
+            ));
+            //$cart->updateQty($this->faker->numberBetween(1, 3), $i);
         }
+
+        /*for ($i = 0; $i < $this->faker->numberBetween(1, 5); ++$i) {
+            //$randomProduct = $this->faker->randomElement($productIds);
+
+
+        }*/
 
         return $cart;
     }
